73d64042ac0e4fae896216310f2b0847f117e845
levi <whil0012@gmail.com>
2019-12-20 23:48:14 -0500

    Day01 part 1 solution

---
+++ AdventOfCode2019.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode2019.Day01;
+
+namespace AdventOfCode2019.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteDay01(args);
+        }
+
+        private static void ExecuteDay01(string[] args)
+        {
+            IEnumerable<int> moduleMasses = GetModuleMasses(args);
+            int fuelTotal = GetFuelTotal(moduleMasses);
+            Print($"Fuel Total: {fuelTotal}");
+        }
+
+        private static void Print(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        private static int GetFuelTotal(IEnumerable<int> moduleMasses)
+        {
+            FuelCalculator fuelCalculator = new FuelCalculator();
+            FuelCounterUpper fuelCounterUpper = new FuelCounterUpper(fuelCalculator);
+            return fuelCounterUpper.CountFuel(moduleMasses);
+        }
+
+        private static IEnumerable<int> GetModuleMasses(string[] args)
+        {
+            string inputFileContents = ReadFileFromCommandLine(args);
+            IEnumerable<int> moduleMasses = inputFileContents
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+            return moduleMasses;
+        }
+
+        private static string ReadFileFromCommandLine(string[] args)
+        {
+            string fileName = args[0];
+            return File.ReadAllText(fileName);
+        }
+    }
+}
